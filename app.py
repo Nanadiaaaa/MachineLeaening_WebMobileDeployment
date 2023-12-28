950f8714d975a74ba2a93eb83552757775d71de7
Nadia <aidannanad2402@gmail.com>
2023-12-28 19:50:10 +0700

    fixing

--- app.py
+++ app.py
@@ -15,40 +15,39 @@ class_list = {
 # Create a Flask web application
 app = Flask(__name__)
 
-# Define a route for the home page
 @app.route('/')
 def home():
+    return render_template('awal.html')
+
+@app.route('/index')
+def next():
     return render_template('index.html')
 
-# Define a route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         try:
-            # Coba untuk memuat model
             loaded_model = TabNetClassifier()
             loaded_model.load_model(model_path)
         except FileNotFoundError:
-            # Tangani kesalahan jika file tidak ditemukan
             return render_template('error.html', message="Model file not found.")
 
-        # Dapatkan data input dari form
+        # Mengambil data input dari form
         age = float(request.form['age'])
         edu = float(request.form['education'])
         ocu = float(request.form['occupation'])
         hours = float(request.form['hours_per_week'])
         country = float(request.form['native_country'])
         start = time.time()
-        # Lakukan prediksi menggunakan model yang dimuat
+
+        # Lakukan prediksi 
         probabilities = loaded_model.predict(np.array([[age, edu, ocu, hours, country]]))
         runtimes = round(time.time() - start, 4)
-        # Konversi list ke string sebelum mengembalikannya
         result = probabilities.tolist()[0]
         prediction_label = list(class_list.keys())[result]
 
-        # Tampilkan prediksi di halaman hasil
         return render_template('/predict.html', prediction=prediction_label, runtime=runtimes)
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     # Jalankan aplikasinya
     app.run(debug=True)
